c0c8a10ca087e79ad346fd203207b933747caac4
Raul Carlos <raulcarlos96@hotmail.com>
2025-02-06 13:00:52 -0300

    feat: funcionalidade de Listar clientes agendados

--- src/routes.ts
+++ src/routes.ts
@@ -13,6 +13,7 @@ import { CountHaircutsController } from "./controllers/haircut/CountHaircutsCont
 import { DetailHaircutController } from "./controllers/haircut/DetailHaircutController";
 
 import { NewScheduleController } from "./controllers/schedule/NewScheduleController";
+import { ListScheduleController } from "./controllers/schedule/ListScheduleController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -46,5 +47,6 @@ router.get(
 
 // --- ROTA SCHEDULE / SERVIÇOS ---
 router.post("/schedule", isAuthenticated, new NewScheduleController().handle);
+router.get("/schedule", isAuthenticated, new ListScheduleController().handle);
 
 export { router };
